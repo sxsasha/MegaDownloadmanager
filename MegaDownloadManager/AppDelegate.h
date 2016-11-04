a39549871a69b29f283e12c201ecc02ae198fbdb
admin <admin@imac-admin-8.local>
2016-11-04 09:24:32 +0200

    Initial Commit

---
+++ MegaDownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MegaDownloadManager
+//
+//  Created by admin on 04.11.16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MegaDownloadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MegaDownloadManager
+//
+//  Created by admin on 04.11.16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
